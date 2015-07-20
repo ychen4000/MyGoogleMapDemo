8a6e348b24ba6f9056eee4da426b406c1a744239
Jin Chen <ychen4000@hotmail.com>
2015-07-20 23:18:00 +1200

    Post, takephoto and select photo buttons
    next, implement the functions of these three buttons

--- MyLemon/app/src/main/java/MyFragments/PostFragment_2.java
+++ MyLemon/app/src/main/java/MyFragments/PostFragment_2.java
@@ -1,10 +1,12 @@
 package MyFragments;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.administrator.mylemon.R;
 
@@ -13,10 +15,49 @@ import com.example.administrator.mylemon.R;
  */
 public class PostFragment_2 extends Fragment {
 
+    Button PostMarker = null;
+    Button TakePhoto = null;
+    Button SelectPhoto = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+     View v = inflater.inflate(R.layout.fragment_post, container, false);
+
+        PostMarker =(Button)v.findViewById(R.id.button_postmarker);
+        TakePhoto = (Button)v.findViewById(R.id.button_takephoto);
+        SelectPhoto = (Button)v.findViewById(R.id.button_selectphoto);
+
+        PostMarker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Todo, 调用 GPS获得 当前位置信息
+              // TODO  动态加入 Maker到 Map Fragment， 同时加入 到 后台 数据库
+                // Todo 方案二，加入数据 到 后台，然后 从 后台 调用
+
+            }
+        });
+
+        TakePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO  camera API，Open camera，and take photo, save for later use.
+
+
+            }
+        });
+
+        SelectPhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO  Open album, select a photo and load for later use.
+
+
+            }
+        });
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        return v;
+
+
     }
 }
